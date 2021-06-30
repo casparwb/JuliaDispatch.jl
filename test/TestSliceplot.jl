fee76cdad01210b3b006e8b6ac2f677760177609
Caspar William Bruenech <c.w.bruenech@pm.me>
2021-06-30 13:52:39 +0200

    fix test data location

--- test/TestSliceplot.jl
+++ test/TestSliceplot.jl
@@ -2,7 +2,7 @@ module TestGraphics
     using Test
     using JuliaDispatch.Dispatch, JuliaDispatch.Graphics
 
-    snap = snapshot(0, data="data/orz/")
+    snap = snapshot(0, data="data/orz/data")
     @test sliceplot(snap, z=0.1, iv="d")
 
 end

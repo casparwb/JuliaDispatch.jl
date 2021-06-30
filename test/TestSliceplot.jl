f946e9df34b990e5c0a4d42d9463eeb2fd1c9b3d
Caspar William Bruenech <c.w.bruenech@pm.me>
2021-06-30 14:04:15 +0200

    fix `TestSliceplot.jl`

--- test/TestSliceplot.jl
+++ test/TestSliceplot.jl
@@ -1,8 +1,7 @@
 module TestGraphics
-    using Test
     using JuliaDispatch.Dispatch, JuliaDispatch.Graphics
 
-    snap = snapshot(0, data="data/orz/data")
-    @test sliceplot(snap, z=0.1, iv="d")
+    snap = snapshot(0, data="data/orz/data");
+    sliceplot(snap, z=0.1, iv="d");
 
 end
